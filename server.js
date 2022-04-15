71bcfd734b82ccfdf8b9621d5c34682cd3b08bd4
Facundo <facundoveliz9@gmail.com>
2022-04-15 19:10:19 -0300

    removed client deployment

--- server.js
+++ server.js
@@ -7,13 +7,5 @@ const path = require("path");
 require("./startup/routes")(app);
 require("./startup/db")();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
