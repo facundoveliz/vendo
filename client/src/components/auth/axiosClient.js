f7e616f54c03dde18cb0b56731287b9fc51e073f
Facundo Veliz <68577124+facundoveliz@users.noreply.github.com>
2022-05-16 15:36:29 -0300

    update timeout

--- client/src/components/auth/axiosClient.js
+++ client/src/components/auth/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 2000,
+  timeout: 60000,
 });
 
 axiosClient.interceptors.request.use(
