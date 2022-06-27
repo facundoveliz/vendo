d5e27c9857545b9caa5227d45a0056b670e956f9
Facundo <facundoveliz9@gmail.com>
2022-06-26 22:31:24 -0300

    not found styles

--- client/src/components/core/NotFound.jsx
+++ client/src/components/core/NotFound.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div>
+    <div className="not-found">
       <h1>404</h1>
       <h2>Not Found</h2>
       <p>
-        We&quot;re sorry, the page you requested could not be found. Please go
+        We&apos;re sorry, the page you requested could not be found. Please go
         back to the home page our contact us.
       </p>
       <Link to="/">
