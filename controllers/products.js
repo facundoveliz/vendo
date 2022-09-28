7f2e7fbfc041612efc5c12c32861d45bf99233c4
Facundo <facundoveliz9@gmail.com>
2022-09-28 14:36:47 -0300

    changed secret key env variable name

--- controllers/products.js
+++ controllers/products.js
@@ -11,7 +11,7 @@ const router = express.Router()
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
+const secretAccessKey = process.env.AWS_SECRET
 
 const s3 = new S3({ region, accessKeyId, secretAccessKey })
 
